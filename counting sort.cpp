86574aa08241a19c95c7a7de4a5b31f2fd3317a1
Jannatul Ferdous Tania <taniajannat206@gmail.com>
2025-09-25 11:53:04 +0600

    Add files via upload

---
+++ counting sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    int maxVal = 0;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(arr[i] > maxVal) maxVal = arr[i];
+    }
+
+    int count[maxVal + 1] = {0};
+
+
+    for(int i = 0; i < n; i++)
+        count[arr[i]]++;
+
+
+    for(int i = 0; i <= maxVal; i++) {
+        while(count[i] > 0) {
+            cout << i << " ";
+            count[i]--;
+        }
+    }
+    cout << endl;
+    return 0;
+}
